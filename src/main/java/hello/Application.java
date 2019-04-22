78ee8c38e5493c573b3a32be15867827c34d4507
Rupesh2010 <nclexgirl8@gmail.com>
2019-04-22 18:02:25 -0400

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,6 +3,8 @@ package hello;
 
 import java.util.Arrays;
 
+// hello
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
