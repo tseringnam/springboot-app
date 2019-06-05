04932c086ba90ac2889cff9321d6a9d0290809ec
tseringnam <35940410+tseringnam@users.noreply.github.com>
2019-06-05 15:20:51 -0400

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!. This is a page maintained by TSERING Namgyal ";
+        return "Greetings from Spring Boot!. This is a page maintained by TSERING Namgyal for New York ";
     }
 
 }
